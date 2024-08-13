ca7934147ca149eaf831bbf00d84052edf36e994
Ankur Verma <ankurverma7619@gmail.com>
2024-08-13 20:10:42 +0530

    Http Methods

---
+++ route-handlers/src/app/comments/[id]/route.js
@@ -0,0 +1,26 @@
+import { comments } from "../data";
+import { NextRequest } from "next/server";
+import { redirect } from "next/navigation";
+export async function GET(Request, { params }) {
+    if (parseInt(params.id) > comments.length) {
+        redirect('/comments')
+    }
+    const comment = comments.find((comment) => comment.id === parseInt(params.id))
+    return Response.json(comment);
+}
+
+export async function PATCH(Request, { params }) {
+    const body = await Request.json()
+    const { text } = body
+    const index = comments.findIndex((comment) => comment.id === parseInt(params.id))
+    comments[index].text = text
+    return Response.json(comments[index])
+}
+
+export async function DELETE(Request, { params }) {
+    const index = comments.findIndex((comment) => comment.id === parseInt(params.id))
+    const deletedComment = comments[index]
+    comments.splice(index, 1);
+    return Response.json(deletedComment)
+
+}
